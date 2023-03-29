97909478ea14f95d01f8daf60a1508226f22b7c3
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-03-29 19:17:24 +0530

    completed basic game

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
-const cards = document.getElementsByClassName(".card");
+let score=0;
 
-// console.log("hkhj");
+let sc=document.querySelector(".score");
+sc.innerText="Score: " + score;
+let cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
-});
+let firstCard=null;
+let secondCard=null;
+let flipped = false;
 
-var flipCard = () => {
-  console.log("jhjh");
+cards.forEach((e) => e.addEventListener("click", flipCard));
 
-  this.classList.add(".flip");
-};
+function flipCard() {
+  if(secondCard===null){
+    this.classList.add("flip");
+    if (!flipped) {
+      firstCard = this;
+      flipped = true;
+    } else {
+      secondCard = this;
+      chek();
+    }
+  }
+}
+
+function updateScore(){
+  score++;
+  sc.innerText="Score: " + score;
+}
+
+function chek() {
+  if (firstCard.dataset.image === secondCard.dataset.image) success();
+  else fail();
+}
+function success() {
+  // console.log("succes");
+  updateScore();
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    reset();
+  }, 1000);
+  // firstCard.removeEventListener("click", flipCard);
+  // secondCard.removeEventListener("click", flipCard);
+}
+
+function fail() {
+  //  console.log("fail");
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    reset();
+  }, 1000);
+}
+
+function reset() {
+  flipped = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle(){
+    cards.forEach((e)=>{
+        let index=Math.floor(Math.random()*16);
+        e.style.order=index;
+    })
+})();
