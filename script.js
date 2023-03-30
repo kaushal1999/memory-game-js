4b3f3c8b4106bba234f9d650a7fdd69cf1d3ebed
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-03-30 15:51:35 +0530

    changed functions to arrow functions

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-let score=0;
+let score = 0;
 
-let sc=document.querySelector(".score");
-sc.innerText="Score: " + score;
+let sc = document.querySelector(".score");
+sc.innerText = "Score: " + score;
 let cards = document.querySelectorAll(".card");
 
-let firstCard=null;
-let secondCard=null;
+let firstCard = null;
+let secondCard = null;
 let flipped = false;
 
-cards.forEach((e) => e.addEventListener("click", flipCard));
-
-function flipCard() {
-  if(secondCard===null){
-    this.classList.add("flip");
+let flipCard = (e) => {
+  // console.log(e);
+  if (secondCard === null) {
+    e.classList.add("flip");
     if (!flipped) {
-      firstCard = this;
+      firstCard = e;
       flipped = true;
     } else {
-      secondCard = this;
+      secondCard = e;
       chek();
     }
   }
-}
+};
 
-function updateScore(){
+cards.forEach((e) => e.addEventListener("click", () => flipCard(e)));
+
+let updateScore = () => {
   score++;
-  sc.innerText="Score: " + score;
-}
+  sc.innerText = "Score: " + score;
+};
 
-function chek() {
+let chek = () => {
   if (firstCard.dataset.image === secondCard.dataset.image) success();
   else fail();
-}
-function success() {
+};
+let success = () => {
   // console.log("succes");
   updateScore();
   setTimeout(() => {
@@ -42,26 +43,26 @@ function success() {
   }, 1000);
   // firstCard.removeEventListener("click", flipCard);
   // secondCard.removeEventListener("click", flipCard);
-}
+};
 
-function fail() {
+let fail = () => {
   //  console.log("fail");
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
     reset();
   }, 1000);
-}
+};
 
-function reset() {
+let reset=()=> {
   flipped = false;
   firstCard = null;
   secondCard = null;
 }
 
-(function shuffle(){
-    cards.forEach((e)=>{
-        let index=Math.floor(Math.random()*16);
-        e.style.order=index;
-    })
+let shuffle=(()=> {
+  cards.forEach((e) => {
+    let index = Math.floor(Math.random() * 16);
+    e.style.order = index;
+  });
 })();
